61090c007ac6c5fc29a4083fb5f0c2af316978e6
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-17 12:49:33 +0000

    Formatted with Prettier

--- apps/ui/pages/_app.js
+++ apps/ui/pages/_app.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
