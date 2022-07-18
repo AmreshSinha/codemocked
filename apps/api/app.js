15b77d6a7458ca3c2b4c1641e78c96d48e25c8bf
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-18 13:37:40 +0000

    Remove default index.html

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,5 +1,4 @@
 var express = require('express')
-var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var cors = require('cors')
@@ -15,7 +14,6 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
