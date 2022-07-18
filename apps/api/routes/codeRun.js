74b935502073aa77f428a4932b56e6cddc977243
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-18 12:29:29 +0000

    Add basic boiler code

---
+++ apps/api/routes/codeRun.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var router = express.Router()
+var genFile = require('../utils/genFile')
+
+/* POST code run. */
+router.post('/', async function (req, res, next) {
+  const { language = 'cpp', code } = req.body
+  if (code === undefined) {
+    return res.status(400).json({ success: false, error: "code body is missing or empty!" });
+  }
+  const filepath = await genFile(language, code);
+  return res.json(req.body)
+})
+
+module.exports = router
