3f5f409d1a5fe293cc3d162fcc47ab81daa273b5
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-19 14:17:13 +0000

    Add prisma client to coreRun route

--- apps/api/routes/codeRun.js
+++ apps/api/routes/codeRun.js
@@ -1,17 +1,35 @@
 var express = require('express')
 var router = express.Router()
 var genFile = require('../utils/genFile')
+const { PrismaClient } = require('@prisma/client')
+const addCodeToQueue = require('../utils/queue.js')
+
+const prisma = new PrismaClient()
 
 /* POST code run. */
-router.post('/', async function (req, res, next) {
-  const { language = 'cpp', code } = req.body
+// router.post('/', async function (req, res, next) {})
+router.post('/', async function (req, res) {
+  const { language = 'cpp', code, input } = req.body
+
   if (code === undefined) {
     return res
       .status(400)
       .json({ success: false, error: 'code body is missing or empty!' })
   }
+
   const filepath = await genFile(language, code)
-  return res.json(req.body)
+
+  const job = await prisma.CodeJob.create({
+    data: {
+      language,
+      filepath,
+      input,
+      status: 'pending',
+    },
+  })
+  const jobId = job('id')
+  addCodeToQueue(jobId)
+  res.status(201).json({ jobId })
 })
 
 module.exports = router
